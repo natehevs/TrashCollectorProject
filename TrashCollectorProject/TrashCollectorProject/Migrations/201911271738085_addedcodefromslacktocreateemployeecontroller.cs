32fc101e45d2e96da437a3a6cdaeffbedf4e3345
patrick hevrdejs <nathanhevrdejs@gmail.com>
2019-11-27 12:04:19 -0600

    got employee after registration working

---
+++ TrashCollectorProject/TrashCollectorProject/Migrations/201911271738085_addedcodefromslacktocreateemployeecontroller.cs
@@ -0,0 +1,16 @@
+namespace TrashCollectorProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedcodefromslacktocreateemployeecontroller : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
